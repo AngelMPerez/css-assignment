3c363bb165ff0e7f679b7b0f90bb4536662e7e77
Angel Perez <angelmperezcarranza@gmail.com>
2019-10-14 16:19:31 -0600

    new js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 function clickHandler(event){
+
   event.preventDefault();
-  var x = document.forms["myForm"]["fname"].value;
+    var x = document.forms["myForm"]["fname"].value;
     document.getElementById("insert").innerHTML = x;
 
     alert("Hello, my name is " + x);
   }
+  console.log('file loaded')
+
