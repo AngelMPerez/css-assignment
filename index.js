4cb7d67c87cb41597caffd5492c73bf8c8bd2bf7
Angel Perez <angelmperezcarranza@gmail.com>
2019-10-14 16:18:16 -0600

    New js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function clickHandler(event){
+  event.preventDefault();
+  var x = document.forms["myForm"]["fname"].value;
+    document.getElementById("insert").innerHTML = x;
+
+    alert("Hello, my name is " + x);
+  }
